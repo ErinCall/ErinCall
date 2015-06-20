5c49b6a3dd5b336bb5823b33ee4eb38ad6617bfb
Erin Call <hello@erincall.com>
2015-06-20 16:42:32 -0700

    Simplify using the `page` helper

--- src/Site.hs
+++ src/Site.hs
@@ -20,18 +20,18 @@ import           Snap.Util.FileServe
 import           Application
 import           Splices
 
-page :: ByteString -> Handler App App ()
-page template = method GET $ render template
+page :: ByteString -> (ByteString, Handler App App ())
+page name = (B.concat ["/", name], method GET $ render name)
 
 routes :: [(ByteString, Handler App App ())]
-routes = [ ("/resume",                    page "resume")
-         , ("/small_languages",           page "small_languages")
-         , ("/secrets_in_source_control", page "secrets_in_source_control")
-         , ("/pgp",                       page "pgp")
-         , ("/about_transgender",         page "about_transgender")
-         , ("/robots.txt",                serveFile "static/robots.txt")
-         , ("/",                          ifTop $ page "index")
-         , ("/static",                    serveDirectory "static")
+routes = [ page "resume"
+         , page "small_languages"
+         , page "secrets_in_source_control"
+         , page "pgp"
+         , page "about_transgender"
+         , ("/robots.txt", serveFile "static/robots.txt")
+         , ("/",           ifTop $ method GET $ render "index")
+         , ("/static",     serveDirectory "static")
          ]
 
 ------------------------------------------------------------------------------
